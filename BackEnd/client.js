0721f65e6487941569aa92e7bcb50d29173e7086
Mario Rocha <mariorocha@Marios-MacBook-Air.local>
2025-04-07 17:18:37 -0600

    Updated files and added new functionality

--- BackEnd/client.js
+++ BackEnd/client.js
@@ -1,17 +1,35 @@
+// client.js
+
+import express from 'express';
 import pkg from 'pg';
+
 const { Client } = pkg;
 
-//
-// const client = new Client({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'API',
-//     password: 'roc23501',
-//     port: 5432,
-//   });
-  
-  const connectClient = async () => {
-    await client.connect();
-  };
-  
-  export { client, connectClient };
+// ✅ Declaramos una sola vez client
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ProyectoBD2',
+  password: 'roc23501',
+  port: 5432,
+});
+
+const app = express();
+const port = 3010;
+
+// ✅ Nos conectamos a la base de datos
+client.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL 🚀');
+
+
+    // ✅ Arrancamos el servidor
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en el puerto ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error de conexión a PostgreSQL ❌', err.stack);
+  });
+
+
